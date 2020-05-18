0587feed8779e39d43f646763eed277065a76568
Jimit Patel <40808237+jimit7@users.noreply.github.com>
2020-05-18 17:21:35 -0400

    Update time24.cpp

--- #include <bits/stdc++.h> #include <string> using namespace std;/*  * Complete the timeConversion function below.  */time24.cpp
+++ #include <bits/stdc++.h> #include <string> using namespace std;/*  * Complete the timeConversion function below.  */time24.cpp
@@ -6,8 +6,9 @@ using namespace std;
  * Complete the timeConversion function below.
  */
 string timeConversion(string s) {
+   int size = s.length();
     string c;
-      string r(2);
+      string r =s.substr(0,2);
     vector<int> t_c(12);
     for(int i=1;i<=12;i++)
     {
@@ -20,41 +21,37 @@ string timeConversion(string s) {
             t_c[i] = 00;
         }
     }
-    if(s[8]=='P')
+    if(s[size-2]=='P')
     {
       if(s[0]!='1'&& s[1]!='2')
       {
-      r[0],r[1]=s[0],s[1];
-      long n=atol(r);
-      r= std::to_string(t_c[n]);
+      //r[0],r[1]=s[0],s[1];
+      int n=stoi(r);
+      string r2= std::to_string(t_c[n]);
+          string r3 =r2.append(s.substr(2,6));
+          return r3;
       }
         if(s[0]=='1'&& s[1]=='2')
         {
-             r[0],r[1]=s[0],s[1];
+            return s.substr(0,size-2);
         }
     }
-    if(s[8]=='A' )
+    if(s[size-2]=='A' )
     {
         if(s[0]!='1'&& s[1]!='2')
       {
-          r[0],r[1]=s[0],s[1];
+         return s.substr(0,size-2);
       }
         if(s[0]== '1'&& s[2]== '2')
         {
-            r[0] =='0';
-            r[1]=='0';
+        string hh = "00";
+        string nTime = hh.append(s.substr(2,6));
+        return nTime;
+            
         }
      // int n = ator   
     }
     
-    c[0] =r[0];
-    c[1]=r[1];
-    for(int i=2;t<8;i++)
-    {
-      c[i]=s[i];  
-    }
-    
-return c;
 }
 
 int main()
