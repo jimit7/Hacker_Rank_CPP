de267f67344f321df14b87705a634a0cc5256ebc
Jimit Patel <40808237+jimit7@users.noreply.github.com>
2020-11-11 20:14:21 -0500

    Update maps.cpp

--- maps.cpp
+++ maps.cpp
@@ -10,12 +10,12 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    map<string,long> pbk;
+    map<std::string,int> pbk;
     int n;
     cin>>n;
     for(int i=0;i<n;i++)
     {
-        string a;
+        std::string a;
         long b;
         cin>>a;
         cin>>b;
@@ -23,15 +23,15 @@ int main() {
     } 
     while(1)
     {
-        string c;
+       std::string c;
         cin>>c;
-        std::map<string,long>::iterator it;
-        it = pbk.find(c);
-  if (it != pbk.end())
-    pbk.erase (it);
+        std::map<std::string,int>::iterator it;
+       // it = pbk.find(c);
+       // if (it != pbk.end())
+        //pbk.erase (it);
 
     
-std::cout << "a => " << pbk.find(c)->first << '\n';
+std::cout << "a => " << pbk.find(c)->second << '\n';
      }
     return 0;
 }
